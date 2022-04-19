185435fd6e9dca60ea2d5eaefa0e5f2f38e97d2c
Anahid <anahidhemmat2130@gmail.com>
2022-04-19 16:29:24 +0400

    Rendering static files

--- myApp/server.js
+++ myApp/server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 
 const app = express();
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 // app.use(logger);
 
-app.get("/", (req, res) => {
-  console.log("here");
-  //   res.send("<h1>Hi Baby</h1>");
-  //   res.sendStatus(500);
-  //   res.status(500).send("Hi");
-  //   res.json({ message: "Eroor" });
-  //   res.download("server.js");
-  res.render("index", { text: "Worldd" });
-});
+// app.get("/", (req, res) => {
+//   console.log("here");
+//   //   res.send("<h1>Hi Baby</h1>");
+//   //   res.sendStatus(500);
+//   //   res.status(500).send("Hi");
+//   //   res.json({ message: "Eroor" });
+//   //   res.download("server.js");
+//   res.render("index", { text: "Worldd" });
+// });
 
 const userRouters = require("./routes/users.js");
 
